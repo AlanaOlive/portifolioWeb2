75d1c3ca382cf5289fb5e0feed5a3df327120aad
oliveira15 <106712504+AlanaOlive@users.noreply.github.com>
2024-12-18 23:23:52 -0300

    estrutura do banco de dados

--- controllers/database/creates.sql
+++ controllers/database/creates.sql
@@ -2,28 +2,47 @@ CREATE DATABASE portifolio_web2;
 
 USE portifolio_web2;
 
-CREATE TABLE usuarios(
+CREATE TABLE users(
 	id INT PRIMARY KEY,
 	user_name VARCHAR(50),
 	adm_roles BOOL
 );
 
-CREATE TABLE projetos(
+CREATE TABLE projects(
 	id INT PRIMARY KEY,
 	project_name VARCHAR(50),
 	project_resume VARCHAR(180)	
+	active BOOL,
+	last_update datetime
 );
 
-
-CREATE TABLE projetos_palavras_chave(
+CREATE TABLE keyword_projects(
 	id INT PRIMARY KEY,
 	id_project INT,
 	keyword VARCHAR(20)
+	active BOOL,
+	last_update datetime
 );
 
-ALTER TABLE `projetos_palavras_chave` ADD CONSTRAINT `fk_id_projeto` FOREIGN KEY (`id_project`) REFERENCES `projetos` (`id`);
-
-CREATE TABLE projetos_autores(
+CREATE TABLE authors_projects(
 	id INT PRIMARY KEY,
-	
+	id_author int,
+	id_project int
+	active BOOL,
+	last_update datetime
+
+);
+
+CREATE TABLE knowledges(
+	id INT PRIMARY KEY,
+	description VARCHAR(50),
+	active BOOL,
+	last_update datetime
+);
+
+CREATE TABLE users_knowledges(
+	id INT PRIMARY KEY,
+	id_user INT,
+	active BOOL,
+	last_update datetime
 );
